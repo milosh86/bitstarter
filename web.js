c4ff77161019575074e3f6d83c72efc93d81eafa
Milos Dzepina <milos.dzepina@gmail.com>
2013-07-19 14:36:29 +0000

    HW4 Bitstarter website

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 
+
 var app = express.createServer(express.logger());
 
 var indexFile = fs.readFileSync('./index.html').toString();
@@ -10,7 +11,7 @@ app.get('/', function(request, response) {
   response.send(indexFile);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
